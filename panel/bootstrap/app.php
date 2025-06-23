e3c2c1fbdc581ae4c60f95f0f6cce91810974753
Kadir Bozat <kadir.bozat@tail.com.tr>
2025-06-23 13:45:04 +0300

    test

--- panel/bootstrap/app.php
+++ panel/bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            \Illuminate\Http\Middleware\StartSession::class,
         ])->statefulApi();
         $middleware->validateCsrfTokens(except: [
             '/reportpdf/icmal',
