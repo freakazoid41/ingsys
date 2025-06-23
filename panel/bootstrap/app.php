8ae84ab6a41c00b2338474a8b64880c956fb34b4
Kadir Bozat <kadir.bozat@tail.com.tr>
2025-06-23 13:49:42 +0300

    test

--- panel/bootstrap/app.php
+++ panel/bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(StartSession::class);
+        //$middleware->append(StartSession::class);
 
         $middleware->web(append: [
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
