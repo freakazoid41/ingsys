fc85e2b6efd683bed807bfeafe86ed0ee5bf7b47
Kadir Bozat <kadir.bozat@tail.com.tr>
2026-01-07 21:34:55 +0300

    test

--- panel/app/Helpers/DocumentHelpers.php
+++ panel/app/Helpers/DocumentHelpers.php
@@ -344,8 +344,6 @@ if(!function_exists('parsePut')){
 
         /* PUT data comes in on the stdin stream */
         $putdata = fopen("php://input", "r");
-        print_r($putdata);
-        die;
         /* Open a file for writing */
         // $fp = fopen("myputfile.ext", "w");
 
