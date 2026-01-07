03547dbf843cec5bd8933fe3aa95a0a8ab9d981d
Kadir Bozat <kadir.bozat@tail.com.tr>
2026-01-07 23:00:08 +0300

    fix

--- panel/app/Http/Controllers/DocumentController.php
+++ panel/app/Http/Controllers/DocumentController.php
@@ -10,8 +10,6 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use App\Upload;
 use Illuminate\Support\Facades\Validator;
-use App\Models\User_logs;
-use App\Models\Sys_options;
 
 
 class DocumentController extends Controller
@@ -38,18 +36,6 @@ class DocumentController extends Controller
                     'success' => !empty($res),
                     'data' => $res,
                 ];
-                /*$res = [];
-                if($id != 0){
-                    $res = $model::where('id',$id)->first();
-                }else{
-                    $res = $model::all();
-                }
-				$res = $res->toarray();
-                //get request for data getting
-                $response = [
-                    'success' => !empty($res),
-                    'data' => $res,
-                ];*/
                 break;
             case "POST":
                 $req = $request->all();
@@ -61,8 +47,20 @@ class DocumentController extends Controller
                 ];
                 break;
             case "PUT":
-                $data = parsePut();
-                $res = (new DocumentServiceProvider())->registerContent($request->id,json_decode($data['data'],true),$_FILES);
+                //normally we have a middleware to parse PUT multipart data but in case it fails we fallback here with custom helper
+                $data = $request->all();
+                if(empty($data)){
+                    //not working on apache server
+                    $data = parsePut();
+                }
+
+                $files = $request->files->all();
+                if(empty($files)){
+                    //not working on apache server
+                    $files = $_FILES;
+                }
+                
+                $res = (new DocumentServiceProvider())->registerContent($request->id,json_decode($data['data'],true),$files);
 
                 $response = [
                     'success' => $res['id'] > 0,
@@ -82,103 +80,119 @@ class DocumentController extends Controller
         return response()->json($response);
 	}
 
-    public function getFacility(Request $request){
-        $validateUser = Validator::make($request->all(),[
-            'code'     => 'required',
-        ]);
-
-        if($validateUser->fails()){
-            return response()->json([
-                'success' => false,
-                'message' => 'Missing Parameters',
-                'error'   => $validateUser->errors()
-            ],401);
-        }else{
-            return  response()->json((new DocumentServiceProvider())->getFacility($request->all()));
-        }
-    }
-
-    public function getPersonInventories(Request $request){
-        $validateUser = Validator::make($request->all(),[
-            'code'     => 'required',
-        ]);
-
-        if($validateUser->fails()){
-            return response()->json([
-                'success' => false,
-                'message' => 'Missing Parameters',
-                'error'   => $validateUser->errors()
-            ],401);
-        }else{
-            return  response()->json((new DocumentServiceProvider())->getInventory($request->all()['code']));
-        }
-    }
-
-    public function newVisit(Request $request){
-        $logModel = 'documents';
+    public function transaction(Request $request){
+        $logModel = 'trasnactions';
         
+        /*if(!checkPermRoute($logModel,$request->method())) return response()->json([
+            'success' => false,
+            'msg'     => 'not valid for system user...',
+        ],401);*/
+        
+        if(session('type_key') != 'op-pert-admin' && strtoupper($request->method()) != 'GET') return response()->json([
+            'success' => false,
+            'msg'     => 'not valid for system user...',
+        ],403);
+       
         //$model = 'App\\Models\\Documents';
         switch(strtoupper($request->method())){
-            case "POST":
-                $req  = $request->all();
-                $data = json_decode($req['data'],true);
-                $data['typeKey'] = "op-doc-visit";
+            case "PUT":
+                /*$data = parsePut();
+                
+                $res = (new DocumentServiceProvider())->registerContent($request->id,json_decode($data['data'],true),$_FILES);
 
-                //here we need to check existing visitor record info for last 30 days by phone number and facility id
-                $entities   = array_values($data['dynamicF'])[0]['entities'];
-                $personInfo = (new DocumentServiceProvider())->getPerson($entities,false,$entities['facility_id']);
-                //if person exist just update record else add new visitor record
-                if($personInfo['success'] == true && !isset($personInfo['data']['exited_at'])){
-                    $res = $personInfo;
-                }else{
-                    $res = (new DocumentServiceProvider())->registerContent(0,$data,$request->files->all());
-                }
                 $response = [
                     'success' => $res['id'] > 0,
                     'data' => $res,
+                ];*/
+			    break;
+			case "DELETE":
+                $res =  (new DocumentServiceProvider())->removeTransaction($request->id);
+                $response = [
+                    'success' => $res['success'],
                 ];
                 break;
-            case "PUT":
-                $data = parsePut();
-                $data = json_decode($data['data'],true);
-                $data['typeKey'] = "op-doc-visit";
-                $res = (new DocumentServiceProvider())->registerContent($request->id,$data,$_FILES);
-
-                if($res['id'] > 0 && isset(array_values($data['dynamicF'])[0]['entities']['exited_at'])){
-                    User_logs::create([
-                        'user_id'     => 0,
-                        'grp_code'    => $res['grpCode'],
-                        'relation'    => 'visitors',
-                        'relation_id' => $res['id'],
-                        'type_id'     => Sys_options::select('id')->where('op_key', 'log-visiter-exit')->first()->id,
-                        'description' => json_encode(array(
-                            'desc' => $res['allEntities']['name'].' tesisten çıkış yaptı',
-                        ),JSON_UNESCAPED_UNICODE)
-                    ]);
-                }
-
-                if($res['id'] > 0 && isset(array_values($data['dynamicF'])[0]['entities']['inventory-taken'])){
-                    User_logs::create([
-                        'user_id'     => 0,
-                        'grp_code'    => $res['grpCode'],
-                        'relation'    => 'visitors',
-                        'relation_id' => $res['id'],
-                        'type_id'     => Sys_options::select('id')->where('op_key', 'log-visiter-enter')->first()->id,
-                        'description' => json_encode(array(
-                            'desc' => $res['allEntities']['name'].' tesise giriş yaptı',
-                        ),JSON_UNESCAPED_UNICODE)
-                    ]);
-                }
-
-                $response = [
-                    'success' => $res['id'] > 0,
-                    'data' => $res,
-                ];
-			    break;
         }
 
         
 
         return response()->json($response);
+	}
+
+    public function preparePayment(Request $request){
+        return [
+            'success' => true,
+            'data' => (new DocumentServiceProvider())->paymentInfo(),
+        ];
     }
+
+    public function setPayment(Request $request){
+        $validateUser = Validator::make($request->all(),[
+            'amount'     => 'required',
+            'currency'   => 'required',
+            'note'       => 'required',
+            'period'     => 'required',
+        ]);
+
+        if(session('type_key') != 'op-pert-admin' && strtoupper($request->method()) != 'GET') return response()->json([
+            'success' => false,
+            'msg'     => 'not valid for system user...',
+        ],403);
+
+        if($validateUser->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => 'Missing Parameters',
+                'error'   => $validateUser->errors()
+            ],401);
+        }else{
+            return (new DocumentServiceProvider())->setPayment($request->all(),$request->trans_file);
+        }
+
+        
+    }
+
+    public function setStatus(Request $request){
+        $validateUser = Validator::make($request->all(),[
+            'id'       => 'required',
+            'op_key'   => 'required',
+        ]);
+
+        if($validateUser->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => 'Missing Parameters',
+                'error'   => $validateUser->errors()
+            ],401);
+        }else{
+            return (new DocumentServiceProvider())->setStatus($request->id,$request->op_key,$request->note);
+        }
+    }
+
+    public function getAparments(){
+        return (new ReportServiceProvider())->getAparments();
+    }
+
+    public function setAparments(Request $request){
+        $validateUser = Validator::make($request->all(),[
+            'title'     => 'required',
+        ]);
+
+        if(session('type_key') != 'op-pert-admin' ) return response()->json([
+            'success' => false,
+            'msg'     => 'not valid for system user...',
+        ],403);
+
+        if($validateUser->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => 'Missing Parameters',
+                'error'   => $validateUser->errors()
+            ],401);
+        }else{
+            return (new DocumentServiceProvider())->setAparments($request->all());
+        }
+
+        
+    }
+
 }
